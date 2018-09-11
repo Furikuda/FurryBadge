0e18903137b7446dac113bdf0777275dda5ab035
Furikuda <19654092+Furikuda@users.noreply.github.com>
2018-09-12 00:39:54 +0200

    Get rid of uBit.h and cleanup code

--- source/main.cpp
+++ source/main.cpp
@@ -2,20 +2,25 @@
 #include "mbed.h"
 #include "ble/BLE.h"
 
-MicroBit uBit;
+MicroBitButton buttonA(MICROBIT_PIN_BUTTON_A, MICROBIT_ID_BUTTON_A);
+MicroBitButton buttonB(MICROBIT_PIN_BUTTON_B, MICROBIT_ID_BUTTON_B);
+
+MicroBitMessageBus bus;
+
+MicroBitSerial serial(USBTX, USBRX);
 
 bool ble_ready = false;
 
 const static GapAdvertisingData::Appearance_t APPEARANCE = (GapAdvertisingData::Appearance_t) 0x26dc;
 const static uint8_t COMPLETE_NAME[] = {
     'D', 'E', 'F', 'C', 'O', 'N', 'F', 'u', 'r', 's'};
+
 const static uint8_t BEACON_AWOO[] = {
     0xff, 0x71, 0xa0, 0x11, 0x02, 0x3f, 0x10, 0x02 };
 const static uint8_t BEACON_EMOTE_BOOP[] = {
     0xff, 0x71, 0xb2, 0x00, 0x00, 0x62, 0x6f, 0x6f, 0x70 };
 const static uint8_t BEACON_EMOTE_OWO[] = {
-    0xff, 0x71, 0xb2, 0xe6, 0x21, 0x5e, 0x2e, 0x5e };
-
+    0xff, 0x71, 0xb2, 0xe6, 0x21, 0x6f, 0x77, 0x6f };
 
 /**
  * This function is called when the ble initialization process has failed
@@ -48,9 +53,11 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
     }
 
     ble_ready = 1;
-    uBit.serial.printf("BLE ready\r\n");
 }
 
+/**
+ * Builds an Awoo broadcast Advertising Data package
+ */
 GapAdvertisingData make_awoo_advdata(){
     GapAdvertisingData advData;
     advData.addAppearance(APPEARANCE);
@@ -60,6 +67,9 @@ GapAdvertisingData make_awoo_advdata(){
     return advData;
 }
 
+/**
+ * Builds a Boop emote Advertising Data package
+ */
 GapAdvertisingData make_boop_advdata(){
     GapAdvertisingData advData;
     advData.addAppearance(APPEARANCE);
@@ -69,15 +79,21 @@ GapAdvertisingData make_boop_advdata(){
     return advData;
 }
 
+/**
+ * Builds an "OwO" emote Advertising Data package
+ */
 GapAdvertisingData make_owo_advdata(){
     GapAdvertisingData advData;
     advData.addAppearance(APPEARANCE);
     advData.addFlags(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
     advData.addData(GapAdvertisingData::COMPLETE_LOCAL_NAME, COMPLETE_NAME, sizeof(COMPLETE_NAME));
-    advData.addData(GapAdvertisingData::MANUFACTURER_SPECIFIC_DATA, BEACON_EMOTE_OWO, sizeof(BEACON_EMOTE_OWO)+1);
+    advData.addData(GapAdvertisingData::MANUFACTURER_SPECIFIC_DATA, BEACON_EMOTE_OWO, sizeof(BEACON_EMOTE_OWO));
     return advData;
 }
 
+/**
+ * Sends the passed Advertisnig data package over the blueteeths
+ */
 int send_beacon(GapAdvertisingData advData) {
 
     if (!ble_ready) {
@@ -88,74 +104,53 @@ int send_beacon(GapAdvertisingData advData) {
 
     GapAdvertisingParams advParams(
             GapAdvertisingParams::ADV_NON_CONNECTABLE_UNDIRECTED, // type of advertising
-            1000, // interval in ms
+            // The following 2 values work ok to be sure we send only one beacon
+            2000, // interval in ms
             1 // Timeout in second
     );
-
     ble.gap().setAdvertisingParams(advParams);
+
     ble.gap().setAdvertisingPayload(advData);
 
     /* Start advertising */
-    uBit.serial.printf("Start adv\r\n");
     ble.gap().startAdvertising();
-//    uBit.sleep(3000);
-//    uBit.serial.printf("Stop adv\r\n");
-//    ble.gap().stopAdvertising();
 
     return 0;
 }
 
+/**
+ * Triggered on Button A press
+ */
 void onButtonA(MicroBitEvent e)
 {
-    BLE& ble = BLE::Instance(BLE::DEFAULT_INSTANCE);
-    uBit.serial.printf("OwO\r\n");
-    ble.stopAdvertising();
-    ble.gap().accumulateAdvertisingPayload(
-           GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
-    ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_NON_CONNECTABLE_UNDIRECTED);
-    GapAdvertisingData advData;
-    advData.addAppearance(APPEARANCE);
-    advData.addFlags(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
-    advData.addData(GapAdvertisingData::COMPLETE_LOCAL_NAME, COMPLETE_NAME, sizeof(COMPLETE_NAME));
-    advData.addData(GapAdvertisingData::MANUFACTURER_SPECIFIC_DATA, BEACON_EMOTE_OWO, sizeof(BEACON_EMOTE_OWO));
-    ble.gap().setAdvertisingPayload(advData);
-    ble.gap().setAdvertisingInterval(200); /* 200ms */
-    ble.gap().startAdvertising();
-    uBit.sleep(1000);
-    ble.gap().stopAdvertising();
-
+    serial.printf("owo\r\n");
+    send_beacon(make_owo_advdata());
 }
 
+/**
+ * Triggered on Button B press
+ */
 void onButtonB(MicroBitEvent e)
 {
-    uBit.serial.printf("BooP\r\n");
+    serial.printf("boop\r\n");
     send_beacon(make_boop_advdata());
-}
-
-void onButtonAB(MicroBitEvent e)
-{
-    uBit.serial.printf("AWOO\r\n");
-    send_beacon(make_awoo_advdata());
 }
 
 int main(void)
 {
-    uBit.serial.printf("\033[0;0H"); // set cursor to 0,0
-    uBit.serial.printf("\033[2J"); // clear screen
+
     BLE& ble = BLE::Instance(BLE::DEFAULT_INSTANCE);
     /* Initialize BLE baselayer, always do this first! */
     ble.init(bleInitComplete);
 
-    // Initialise the micro:bit runtime.
-    uBit.init();
+    /* sets up call backs on button press events */
+    bus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+    bus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
 
-    // Register to receive events when any buttons are clicked, including the A+B virtual button (both buttons at once).
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_AB, MICROBIT_BUTTON_EVT_CLICK, onButtonAB);
+    serial.printf("I have booted :3\r\n");
 
-    // We're done, so just enter a power efficient sleep while we wait for an event.
-    while (1)
-        uBit.sleep(10000);
-
+    // We're done
+    while (1){
+        // Todo, have a better 'sleep'
+    }
 }
